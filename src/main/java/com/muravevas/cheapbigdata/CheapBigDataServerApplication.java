01cfe8eb92c8fe1534015e03d60a86d3ef8478d6
muravevas <muravevartem@outlook.com>
2023-03-07 20:45:48 +0400

    Initial commit

---
+++ src/main/java/com/muravevas/cheapbigdata/CheapBigDataServerApplication.java
@@ -0,0 +1,13 @@
+package com.muravevas.cheapbigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheapBigDataServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheapBigDataServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muravevas/cheapbigdata/CheapBigDataServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muravevas.cheapbigdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheapBigDataServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
